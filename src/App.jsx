ae6be0af6a0d8d078dc239378c401ee6d4239fc4
LeadingSocial <willuk15@googlemail.com>
2025-07-16 21:02:33 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to LeadingSocial 🚀</h1>
+      <p>Your automated AI marketing assistant</p>
+    </div>
+  );
+}
+export default App;
