f2e2fa85a2abf17da7166d1b7bd29c31b40707ac
LeadingSocial <willuk15@googlemail.com>
2025-07-16 21:22:56 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>Welcome to LeadingSocial</h1>
+      <p>This is your Vite + React landing page.</p>
+    </div>
+  );
+}
+
+export default App;
