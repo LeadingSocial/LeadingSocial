77bdf95267176a2a8d6186c37676ff1e03c888dc
LeadingSocial <willuk15@googlemail.com>
2025-07-16 11:00:52 +0100

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to LeadingSocial</h1>
+      <p>This is the starting point of your AI-powered social media automation platform.</p>
+    </div>
+  );
+}
+
+export default App;
